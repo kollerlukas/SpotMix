37ac782a517380d1d4088340e7de875b116ce6ec
Le0198 <lopezleonardo198@gmail.com>
2019-11-15 15:28:46 -0600

    Fixed up create party screen

--- app/src/main/java/edu/illinois/cs465/spotmix/activities/CreatePartyActivity.java
+++ app/src/main/java/edu/illinois/cs465/spotmix/activities/CreatePartyActivity.java
@@ -2,12 +2,20 @@ package edu.illinois.cs465.spotmix.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 import edu.illinois.cs465.spotmix.R;
 import edu.illinois.cs465.spotmix.api.firebase.FirebaseHelper;
@@ -18,10 +26,12 @@ import edu.illinois.cs465.spotmix.api.spotify.SpotifyHelper;
 import edu.illinois.cs465.spotmix.api.spotify.models.User;
 
 public class CreatePartyActivity extends AppCompatActivity
-        implements View.OnClickListener, FirebaseHelper.CreateCallback {
+        implements View.OnClickListener, FirebaseHelper.CreateCallback, AdapterView.OnItemSelectedListener {
 
     // spotify helper - testing commit
     private SpotifyHelper helper;
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +40,22 @@ public class CreatePartyActivity extends AppCompatActivity
 
         // get spotify helper from intent
         helper = getIntent().getParcelableExtra(SpotifyHelper.PARCEL_KEY);
+
+        // get spinner from activity_create_party.xml
+        spinner =(Spinner)findViewById(R.id.fallback_playlist_spinner);
+
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> playlistOptions = new ArrayList<String>();
+        playlistOptions.add("Feedback Playlist");
+        playlistOptions.add("Playlist1");
+        playlistOptions.add("Playlist2");
+        playlistOptions.add("Playlist3");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, playlistOptions);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
     }
 
     @Override
@@ -81,5 +107,19 @@ public class CreatePartyActivity extends AppCompatActivity
             // TODO: error handling
             Toast.makeText(this, "Some error...", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        // On selecting a spinner item
+        String item = adapterView.getItemAtPosition(i).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
